7402cfda54175d5b279e12c8eafe535ec965c622
gchudublin <chu_greg@yahoo.com>
2014-05-18 05:29:54 -0400

    test

---
+++ app/src/main/java/com/gregchu/test/t1/app/MainActivity.java
@@ -0,0 +1,30 @@
+package com.gregchu.test.t1.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void onClickMenuOther(MenuItem item) {
+        Toast toast = Toast.makeText(this, "hi other", Toast.LENGTH_LONG);
+    }
+}
